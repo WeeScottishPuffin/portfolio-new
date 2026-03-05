2386fc09cd6728ca7af70fee6283c232b03e5ed2
Naut van der Winden <nwinden@proton.me>
2026-03-05 13:13:06 +0000

    Testing in prod cause cookies aren't working locally

--- script/fetch_commit.js
+++ script/fetch_commit.js
@@ -15,15 +15,20 @@ function updateCommitTracker(json){
 	dt = json["commit"]["author"]["date"];
 	msg = json["commit"]["message"];
 	const ed = new Date();
-	ed.setTime(ed.getTime() + (1000 * 60 * 10));
-	ghcookie = `gh-commit=${dt}${msg};expires=${ed.toUTCString()};`;
-	document.cookie = encodeURI(ghcookie);
+	ed.setTime(ed.getTime() + (1000 * 10));
+
+	cookieStore.set({
+		name:"gh-commit",
+		value:`${dt}${msg}`,
+		expires: Date.now() + (1000 * 10)
+	})
 	COMMIT_DT_OBJ.innerHTML = dt;
 	COMMIT_MSG_OBJ.innerHTML = msg;
 } 
 
 //Function to update the sidebar last commit thing but from the cached data
 function updateCommitTrackerFromCache(res){
+	console.log(res);
 	res = decodeURI(res);
 	dt = res.substring(0,20);
 	msg = res.substring(20);
@@ -33,10 +38,10 @@ function updateCommitTrackerFromCache(res){
 
 //Check cookie for last refresh time
 cookie = document.cookie;
-cookieStore.get("gh-commit").then((res)=>{
-	if (res == null){ //Cookie doesnt exist
+cookieStore.get("gh-commit").then(res=>{
+	if(res == null){
 		getLatestCommit();
 	} else {
-		updateCommitTrackerFromCache(res.value);
+		updateCommitTrackerFromCache(res);
 	}
 })
